c45cfe15ab36f8f6fe4ba53406df8ed81b319b9c
SkySecCoder <prasad.aakash@gmail.com>
2020-02-23 23:55:47 -0500

    ls instances

--- ashura/aws/awsDestroyer.go
+++ ashura/aws/awsDestroyer.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
-	//"github.com/aws/aws-sdk-go/service/ec2"
+	"strings"
+	"encoding/json"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/sts"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func main() {
-	fmt.Println("[+] Running AwsDestroyer...")
+	fmt.Println("[+] Running AwsDestroyer as :")
 	creds := credentials.NewEnvCredentials()
 
 	mysession, _ := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1"),
+		Region:      aws.String("us-east-1"),
 		Credentials: creds,
 	})
 	stsClient := sts.New(mysession)
@@ -22,5 +24,31 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(result)
+
+	Whoami(string(*result.Arn))
+	GetInstances(mysession)
+}
+
+func Whoami(arn string) {
+	user := strings.Split(arn, "/")[len(strings.Split(arn, "/"))-1]
+	account := strings.Split(arn, ":")[4]
+	fmt.Println("\t- User\t\t: "+user)
+	fmt.Println("\t- Account\t: "+string(account))
+	fmt.Println("\t- ARN\t\t: "+string(arn))
+}
+
+func GetInstances(mysession *session.Session) {
+	var instanceList []string
+	ec2Client := ec2.New(mysession)
+	result, err := ec2Client.DescribeInstances(&ec2.DescribeInstancesInput{})
+	if err != nil {
+		panic(err)
+	}
+
+	for instance := range result.Reservations {
+		blob := result.Reservations[instance].Instances
+		instanceList = append(instanceList, *blob[0].InstanceId)
+	}
+	data, _ := json.MarshalIndent(instanceList, "", "    ")
+	fmt.Println(string(data))
 }
