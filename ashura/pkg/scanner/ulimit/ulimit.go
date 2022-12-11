ff527d8e95359f9f97eaede4775cfcde5a068228
SkySecCoder <prasad.aakash@gmail.com>
2022-12-10 23:07:12 -0500

    patch

---
+++ ashura/pkg/scanner/ulimit/ulimit.go
@@ -0,0 +1,20 @@
+package scanner
+
+import (
+    "os/exec"
+    "strings"
+    "strconv"
+)
+
+func Ulimit() int64 {
+    out, err := exec.Command("ulimit", "-n").Output()
+    if err != nil {
+        panic(err)
+    }
+    stringOutput := strings.TrimSpace(string(out))
+    ulimit, err := strconv.ParseInt(stringOutput, 10, 64)
+    if err != nil {
+        panic(err)
+    }
+    return ulimit
+}
